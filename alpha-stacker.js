a70f3ff9ae08d8bf528453dc51c5e58c4dc1f985
Ellis Thomas <ellisthomas80@gmail.com>
2017-02-09 18:57:11 -0600

    Update alpha-stacker.js

--- alpha-stacker.js
+++ alpha-stacker.js
@@ -9,18 +9,18 @@ function stackLetters() {
 }
 stackLetters();
 
-// var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-// function stackLetters() {
-//   var letters = "";
-//   var spaces = "";
-//   for (var i = 0; i < alphabet.length; i++) {
-//     letters += alphabet[i];
-//     spaces = letters.replace(/\s/g, "");
-//     if (spaces.length % 3 === 0) {
-//       letters += " ";
-//     } else {
-//       console.log(letters);
-//     }
-//   }
-// } 
-// stackLetters();
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+function stackLetters() {
+  var letters = "";
+  var spaces = "";
+  for (var i = 0; i < alphabet.length; i++) {
+    letters += alphabet[i];
+    spaces = letters.replace(/\s/g, "");
+    if (spaces.length % 3 === 0) {
+      letters += " ";
+    } else {
+      console.log(letters);
+    }
+  }
+} 
+stackLetters();
