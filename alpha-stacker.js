6929e3f8d215f70ef6698add30acb82ca18ca726
ellisthomas <ellisthomas@gmail.com>
2017-02-11 11:41:44 -0600

    changes to if statement

--- alpha-stacker.js
+++ alpha-stacker.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // function stackLetters() {
 // 	var letters = "";
 // 	var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -8,23 +7,13 @@
 //   }		
 // }
 // stackLetters();
-=======
-// var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-function stackLetters() {
-	var letters = "";
-	var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-	for (var i = 0; i < alphabet.length; i++) {
-	letters += alphabet[i];
-	console.log(letters);
-  }		
-}
-stackLetters();
->>>>>>> a70f3ff9ae08d8bf528453dc51c5e58c4dc1f985
+
+
 
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 function stackLetters() {
   var letters = "";
-<<<<<<< HEAD
+
   
   for (var counter = 0; counter < alphabet.length; counter++) {
     if (counter % 3 === 0) {
@@ -34,17 +23,5 @@ function stackLetters() {
     }
   }
 stackLetters(alphabet);
-=======
-  var spaces = "";
-  for (var i = 0; i < alphabet.length; i++) {
-    letters += alphabet[i];
-    spaces = letters.replace(/\s/g, "");
-    if (spaces.length % 3 === 0) {
-      letters += " ";
-    } else {
-      console.log(letters);
-    }
-  }
-} 
-stackLetters();
->>>>>>> a70f3ff9ae08d8bf528453dc51c5e58c4dc1f985
+
+  
